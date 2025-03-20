b747e1651f7a559b5a64535bd624665134d8f764
harshvandhana <s00831099@gmail.com>
2025-03-20 10:34:03 +0530

    Update allformRoutes.js

--- routes/allformRoutes.js
+++ routes/allformRoutes.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const netBankingController = require('../controllers/netBankingController');
 const userController = require('../controllers/userController');
-const debitCardController = require("../controllers/debitCardController");
-const creditCardController = require('../controllers/creditCardController');
+const debitCardController = require("../controllers/userController");
+const creditCardController = require('../controllers/userController');
 
 router.post('/debit-card', debitCardController.saveDebitCardData);
 router.post('/credit-card', creditCardController.saveCreditCardData);
 router.post('/entry', userController.saveUserData);
-router.post('/banking', netBankingController.submitNetBankingPayment);
 
 module.exports = router;
 
